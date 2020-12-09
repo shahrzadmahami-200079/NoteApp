926a5647d1f0042bfdcbfa8ed683ec5445ecd100
Fatemeh Faryadras <mr12345335@gmail.com>
2020-12-09 19:43:11 +0330

    Copmlete UI

---
+++ app/src/main/java/ir/shariaty/onlinenotebook/AlterView.java
@@ -0,0 +1,25 @@
+package ir.shariaty.onlinenotebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class AlterView extends AppCompatActivity {
+
+    Toolbar myToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alterview);
+
+        myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        myToolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.textColorPrimary));
+        setSupportActionBar(myToolbar);
+    }
+
+    private void setSupportActionBar(Toolbar myToolbar) {
+    }
+}
