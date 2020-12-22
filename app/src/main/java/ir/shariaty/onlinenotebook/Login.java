862929a928ec1f2128f309a6725318a5dc559205
Fatemeh Faryadras <mr12345335@gmail.com>
2020-12-22 22:37:01 +0330

    Change Login-->> edit code

--- app/src/main/java/ir/shariaty/onlinenotebook/Login.java
+++ app/src/main/java/ir/shariaty/onlinenotebook/Login.java
@@ -39,39 +39,45 @@ public class Login extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        registerUser.setOnClickListener(view -> startActivity(new Intent(Login.this, RegisterActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)));
+        registerUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,RegisterActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK));
+            }
+        });
 
-        login.setOnClickListener(view -> {
-            String txt_email = email.getText().toString();
-            String txt_password = password.getText().toString();
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
 
-            if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
-                Toast.makeText(Login.this, "Email or Password is empty!", Toast.LENGTH_SHORT).show();
-            } else {
-                loginUser(txt_email, txt_password);
+                if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                    Toast.makeText(Login.this ,"Email or Password is empty!", Toast.LENGTH_SHORT).show();
+                } else {
+                    loginUser(txt_email,txt_password);
+                }
             }
         });
     }
 
     private void loginUser(String email, String password) {
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                Toast.makeText(Login.this, "Login is Successful", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(Login.this, MainPage.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-                finish();
+        mAuth.signInWithEmailAndPassword(email , password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(Login.this, "Login is Successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Login.this, MainPage.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    finish();
+                }
             }
-        }).addOnFailureListener(e -> Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show());
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        if (FirebaseAuth.getInstance().getCurrentUser() != null){
-            startActivity(new Intent(Login.this,MainPage.class));
-            finish();
-        }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Login.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
